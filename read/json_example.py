5b95c6957d93c52fd252c4986b5a3b542bf22989
Victoria <vsulagina@ozon.ru>
2020-07-09 22:36:23 +0300

    tests examples added

---
+++ read/json_example.py
@@ -0,0 +1,8 @@
+from json import loads, dumps
+
+with open('../data/data.json', 'r') as file:
+    j = file.read()
+    n = loads(j)
+    print(n)
+    print(dumps(n))
+
---
+++ read/read_file.py
@@ -0,0 +1,19 @@
+file = open('data/data.txt', 'r', )
+
+# чтение всего файла
+# print(file.read())
+
+# количество символов
+# print(file.read(20))
+
+# построчно
+# print(file.readline())
+
+# список
+# print(file.readlines())
+
+# построчно
+# for item in file:
+#     print(item)
+
+file.close()
